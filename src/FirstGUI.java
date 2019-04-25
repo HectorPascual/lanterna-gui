31daf7aebb980a9a3ac9a90d076663fcaa75aa6e
Jordi <j.calveres.garcia@gmail.com>
2019-04-25 10:36:11 +0200

    Refactor

--- src/FirstGUI.java
+++ src/FirstGUI.java
@@ -41,185 +41,175 @@ public class FirstGUI {
     private Button exit;
 
     public FirstGUI(MySocket socket){
-        this.socket = socket;
-
+      this.socket = socket;
     }
 
     public void run () {
+      try {
+        term = new UnixTerminal();
+        termColumns = term.getTerminalSize().getColumns();
+        termRows = term.getTerminalSize().getRows();
 
-        try {
+        screen = new TerminalScreen(term);
+        screen.startScreen();
 
-            term = new UnixTerminal();
-            termColumns = term.getTerminalSize().getColumns();
-            termRows = term.getTerminalSize().getRows();
+        textGUI = new MultiWindowTextGUI(screen);
+        window = new BasicWindow("SAD 2019  -   CLIENT CHAT");
+        window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
 
-            screen = new TerminalScreen(term);
-            screen.startScreen();
+        initComponents();
+        sizeComponents();
+        initPanel();
 
-            textGUI = new MultiWindowTextGUI(screen);
-            window = new BasicWindow("SAD 2019  -   CLIENT CHAT");
-            window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
-
-            initComponents();
+        term.addResizeListener(new TerminalResizeListener() {
+          @Override
+          public void onResized(Terminal terminal, TerminalSize terminalSize) {
+            termColumns = terminalSize.getColumns();
+            termRows = terminalSize.getRows();
             sizeComponents();
-            initPanel();
+          }
 
+        });
 
-            term.addResizeListener(new TerminalResizeListener() {
-                @Override
-                public void onResized(Terminal terminal, TerminalSize terminalSize) {
-                    termColumns = terminalSize.getColumns();
-                    termRows = terminalSize.getRows();
-                    sizeComponents();
-                }
+        requestName();
 
-            });
+        KeyStrokeListener listener = new KeyStrokeListener(this);
+        window.addWindowListener(listener);
+        window.setComponent(absolutPanel);
+        textGUI.addWindowAndWait(window);
 
-            requestName();
+      } catch (IOException e) {
 
-            KeyStrokeListener listener = new KeyStrokeListener(this);
-            window.addWindowListener(listener);
-            window.setComponent(absolutPanel);
-            textGUI.addWindowAndWait(window);
-
+      } finally {
+        try {
+          screen.stopScreen();
         } catch (IOException e) {
-
-        } finally {
-            try {
-                screen.stopScreen();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+          e.printStackTrace();
         }
+      }
     }
 
     public void checkKeys(){
-
-        Thread readKeys = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    while (true) {
-
-                        keyStroke = screen.pollInput();
-                        if (keyStroke != null && keyStroke.getKeyType() == KeyType.Escape) {
-                            window.close();
-                            System.exit(0); //exits the process
-                        }
-                    }
-                }catch(IOException e){
-
-                }
+      Thread readKeys = new Thread(new Runnable() {
+        public void run() {
+          try {
+            while (true) {
+              keyStroke = screen.pollInput();
+              if (keyStroke != null && keyStroke.getKeyType() == KeyType.Escape) {
+                window.close();
+                System.exit(0); //exits the process
+              }
             }
-        });
-        readKeys.start();
-
+          }catch(IOException e){
+          }
+        }
+      });
+      readKeys.start();
     }
 
     public void write(String txt){
-        if(countLines() >= termRows*76/100) chatContent.setText("");
-        String tmp = chatContent.getText() + '\n' + txt;
-        chatContent.setText(tmp);
+      if(countLines() >= termRows*76/100) chatContent.setText("");
+      String tmp = chatContent.getText() + '\n' + txt;
+      chatContent.setText(tmp);
     }
 
     public void close(){
-        window.close();
-        System.exit(0); //exits the process
+      window.close();
+      System.exit(0); //exits the process
     }
 
     public void updateUserList(List<String> usersList){
-        String tmp = "";
-        for(String s : usersList){
-          tmp = tmp + s + '\n';
-        }
-        users.setText(tmp);
+      String tmp = "";
+      for(String s : usersList){
+        tmp = tmp + s + '\n';
+      }
+      users.setText(tmp);
     }
 
     private void requestName() {
-        name = TextInputDialog.showDialog(textGUI, "","Type your name (max 8 letters)","");
-        socket.write(name);
+      name = TextInputDialog.showDialog(textGUI, "","Type your name (max 8 letters)","");
+      socket.write(name);
     }
 
     private void sizeComponents() {
-        users.setPosition(new TerminalPosition(termColumns*82/100, 0));
-        users.setSize(new TerminalSize(8, termRows));
+      users.setPosition(new TerminalPosition(termColumns*82/100, 0));
+      users.setSize(new TerminalSize(8, termRows));
 
-        separatorHor.setSize(new TerminalSize(termColumns, 1));
-        separatorHor.setPosition(new TerminalPosition(0, termRows*76/100));
+      separatorHor.setSize(new TerminalSize(termColumns, 1));
+      separatorHor.setPosition(new TerminalPosition(0, termRows*76/100));
 
-        chatContent.setPosition(new TerminalPosition(1, 0));
-        chatContent.setSize(new TerminalSize(termColumns*78/100,termRows*75/100));
+      chatContent.setPosition(new TerminalPosition(1, 0));
+      chatContent.setSize(new TerminalSize(termColumns*78/100,termRows*75/100));
 
-        separatorVert.setSize(new TerminalSize(1, termRows));
-        separatorVert.setPosition(new TerminalPosition(termColumns*80/100, 0));
+      separatorVert.setSize(new TerminalSize(1, termRows));
+      separatorVert.setPosition(new TerminalPosition(termColumns*80/100, 0));
 
-        textBox.setPosition(new TerminalPosition(0, termRows*80/100));
-        textBox.setSize(new TerminalSize(termColumns*79/100, termRows*15/100));
+      textBox.setPosition(new TerminalPosition(0, termRows*80/100));
+      textBox.setSize(new TerminalSize(termColumns*79/100, termRows*15/100));
 
-        send.setPosition(new TerminalPosition(termColumns*85/100, termRows*80/100));
-        send.setSize(new TerminalSize(8, 1));
+      send.setPosition(new TerminalPosition(termColumns*85/100, termRows*80/100));
+      send.setSize(new TerminalSize(8, 1));
 
-        exit.setPosition(new TerminalPosition(termColumns*85/100, termRows*85/100));
-        exit.setSize(new TerminalSize(8, 1));
+      exit.setPosition(new TerminalPosition(termColumns*85/100, termRows*85/100));
+      exit.setSize(new TerminalSize(8, 1));
 
     }
+
     private void initComponents(){
-        users = new Label("");
+      users = new Label("");
 
-        chatContent = new Label("");
+      chatContent = new Label("");
 
-        separatorHor = new Separator(Direction.HORIZONTAL);
+      separatorHor = new Separator(Direction.HORIZONTAL);
 
-        separatorVert = new Separator(Direction.VERTICAL);
+      separatorVert = new Separator(Direction.VERTICAL);
 
-        textBox = new TextBox("", TextBox.Style.MULTI_LINE);
+      textBox = new TextBox("", TextBox.Style.MULTI_LINE);
 
-        send = new Button("Send", new Runnable() {
-            @Override
-            public void run() {
-                sendAction();
-            }
-        });
+      send = new Button("Send", new Runnable() {
+        @Override
+        public void run() {
+          sendAction();
+        }
+      });
 
-        exit = new Button("Exit", new Runnable() {
-            @Override
-            public void run() {
-                window.close();
-                System.exit(0);
-            }
-        });
+      exit = new Button("Exit", new Runnable() {
+        @Override
+        public void run() {
+          window.close();
+          System.exit(0);
+        }
+      });
     }
 
     private void initPanel(){
-        try {
-            absolutPanel = new Panel(new AbsoluteLayout());
-            absolutPanel.setPreferredSize(new TerminalSize(term.getTerminalSize().getColumns(), term.getTerminalSize().getRows()));
-            absolutPanel.addComponent(users);
-            absolutPanel.addComponent(chatContent);
-            absolutPanel.addComponent(separatorHor);
-            absolutPanel.addComponent(separatorVert);
-            absolutPanel.addComponent(textBox);
-            absolutPanel.addComponent(send);
-            absolutPanel.addComponent(exit);
-        } catch (IOException e){
-
-        }
+      try {
+        absolutPanel = new Panel(new AbsoluteLayout());
+        absolutPanel.setPreferredSize(new TerminalSize(term.getTerminalSize().getColumns(), term.getTerminalSize().getRows()));
+        absolutPanel.addComponent(users);
+        absolutPanel.addComponent(chatContent);
+        absolutPanel.addComponent(separatorHor);
+        absolutPanel.addComponent(separatorVert);
+        absolutPanel.addComponent(textBox);
+        absolutPanel.addComponent(send);
+        absolutPanel.addComponent(exit);
+      } catch (IOException e){
+      }
     }
 
     //Utility to clear screen
     private int countLines(){
-        String txt = chatContent.getText();
-        String[] array = txt.split("\n");
-        return array.length+1;
+      String txt = chatContent.getText();
+      String[] array = txt.split("\n");
+      return array.length+1;
     }
 
-
     private void sendAction(){
-        String input = textBox.getText();
-        if(!input.equals("")) {
-            socket.write(input);
-            textBox.setText("");
-            write(input);
-
-        }
+      String input = textBox.getText();
+      if(!input.equals("")) {
+        socket.write(input);
+        textBox.setText("");
+        write(input);
+      }
     }
 }
